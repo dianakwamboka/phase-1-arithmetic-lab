ec8f20ea355ff39d06b1d3971b51cd722e376486
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-08-20 19:22:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+const num1=2
+const num2=31
+const multiply=(num1*num2);
+//Generators random integer greater than 0
+const random= Math.floor(Math.random()*10)
+//calculates a remainder that's equal to 4;
+const num3 =28
+const num4 =6
+const mod=(num3%num4);
+// returning the max number
+const set =[10,15,20]
+const max = Math.max(...set)
+
+
+
+
+
+
+
+
